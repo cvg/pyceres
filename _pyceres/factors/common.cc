8340b3e039358d35dec3bf8bc67d93e88f76d2af
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2022-02-15 14:22:31 +0100

    Formatting

--- _pyceres/factors/common.cc
+++ _pyceres/factors/common.cc
@@ -5,7 +5,7 @@
 
 ceres::CostFunction* CreateNormalPrior(const Eigen::VectorXd& mean,
                                        const Eigen::Matrix<double, -1, -1>& covariance) {
-    THROW_CHECK_EQ(covariance.cols(), mean.size());
-    THROW_CHECK_EQ(covariance.cols(), covariance.rows());
-    return new ceres::NormalPrior(covariance.inverse().llt().matrixL(), mean);
+  THROW_CHECK_EQ(covariance.cols(), mean.size());
+  THROW_CHECK_EQ(covariance.cols(), covariance.rows());
+  return new ceres::NormalPrior(covariance.inverse().llt().matrixL(), mean);
 }
