e3376ede8389828e23f7f2c9af69c81b6b0436c0
Phil26AT <phil.lindenberger@gmail.com>
2022-09-30 12:37:42 +0200

    Add ListIterationCallback definition

--- _pyceres/core/callbacks.cc
+++ _pyceres/core/callbacks.cc
@@ -27,15 +27,13 @@
 //
 // Author: nikolausmitchell@gmail.com (Nikolaus Mitchell)
 // Edited by: philipp.lindenberger@math.ethz.ch (Philipp Lindenberger)
-
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
-namespace py = pybind11;
 
 #include <ceres/ceres.h>
 
-#include "helpers.h"
+namespace py = pybind11;
 
 // Trampoline class so we can create an EvaluationCallback in Python.
 class PyEvaluationCallBack : public ceres::EvaluationCallback {
@@ -43,11 +41,13 @@ class PyEvaluationCallBack : public ceres::EvaluationCallback {
   /* Inherit the constructors */
   using ceres::EvaluationCallback::EvaluationCallback;
 
-  void PrepareForEvaluation(bool evaluate_jacobians, bool new_evaluation_point) override {
-    PYBIND11_OVERLOAD_PURE(void,                      /* Return type */
-                           ceres::EvaluationCallback, /* Parent class */
-                           PrepareForEvaluation,      // Name of function in C++ (fn)
-                           evaluate_jacobians, new_evaluation_point /* Argument(s) */
+  void PrepareForEvaluation(bool evaluate_jacobians,
+                            bool new_evaluation_point) override {
+    PYBIND11_OVERLOAD_PURE(
+        void,                                    /* Return type */
+        ceres::EvaluationCallback,               /* Parent class */
+        PrepareForEvaluation,                    // Name of function in C++ (fn)
+        evaluate_jacobians, new_evaluation_point /* Argument(s) */
     );
   }
 };
@@ -56,24 +56,41 @@ class PyIterationCallback : public ceres::IterationCallback {
  public:
   using ceres::IterationCallback::IterationCallback;
 
-  ceres::CallbackReturnType operator()(const ceres::IterationSummary& summary) override {
-    PYBIND11_OVERRIDE_PURE_NAME(ceres::CallbackReturnType,  // Return type (ret_type)
-                                ceres::IterationCallback,   // Parent class (cname)
-                                "__call__",  // Name of method in Python (name)
-                                operator(),  // Name of function in C++ (fn)
-                                summary);
+  ceres::CallbackReturnType operator()(
+      const ceres::IterationSummary& summary) override {
+    PYBIND11_OVERRIDE_PURE_NAME(
+        ceres::CallbackReturnType,  // Return type (ret_type)
+        ceres::IterationCallback,   // Parent class (cname)
+        "__call__",                 // Name of method in Python (name)
+        operator(),                 // Name of function in C++ (fn)
+        summary);
   }
 };
 
 PYBIND11_MAKE_OPAQUE(std::vector<ceres::IterationCallback*>);
 
 void init_callbacks(py::module& m) {
-  py::class_<ceres::IterationCallback, PyIterationCallback /* <--- trampoline*/>(
-      m, "IterationCallback")
+  py::class_<ceres::IterationCallback,
+             PyIterationCallback /* <--- trampoline*/>(m, "IterationCallback")
       .def(py::init<>())
       .def("__call__", &ceres::IterationCallback::operator());
 
-  py::class_<ceres::EvaluationCallback, PyEvaluationCallBack /* <--- trampoline*/>(
-      m, "EvaluationCallback")
+  py::class_<ceres::EvaluationCallback,
+             PyEvaluationCallBack /* <--- trampoline*/>(m, "EvaluationCallback")
       .def(py::init<>());
+
+  auto vec_it_cb = py::bind_vector<std::vector<ceres::IterationCallback*>>(
+      m, "ListIterationCallback");
+
+  vec_it_cb.def(
+      py::init<>([](py::list list) {
+        std::vector<ceres::IterationCallback*> callbacks;
+        for (auto& handle : list) {
+          callbacks.push_back(handle.cast<ceres::IterationCallback*>());
+        }
+        return callbacks;
+      }),
+      py::keep_alive<1, 2>());
+  py::implicitly_convertible<py::list,
+                             std::vector<ceres::IterationCallback*>>();
 }
