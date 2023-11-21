7f66455063e26d84cda115361d53d697e523dae6
nnop <ihacku@163.com>
2023-11-21 22:44:28 +0800

    Enable cost functions written in Python (#23)
    Co-authored-by: qiang.guo1 <qiang.guo1@nio.com>

---
+++ examples/test_python_cost.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pyceres
+
+
+# ref: examples/helloworld_analytic_diff.cc
+class HelloworldCostFunction(pyceres.CostFunction):
+    def __init__(self):
+        pyceres.CostFunction.__init__(self)
+        self.set_num_residuals(1)
+        self.set_parameter_block_sizes([1])
+
+    def Evaluate(self, parameters, residuals, jacobians):
+        x = parameters[0][0]
+        residuals[0] = 10. - x
+        if jacobians is not None:
+            jacobians[0][0] = -1.
+        return True
+
+
+def test_python_cost():
+    x = np.array(5.0)
+    x_ori = x.copy()
+    prob = pyceres.Problem()
+    cost = HelloworldCostFunction()
+    prob.add_residual_block(cost, None, [x])
+    options = pyceres.SolverOptions()
+    options.minimizer_progress_to_stdout = True
+    summary = pyceres.SolverSummary()
+    pyceres.solve(options, prob, summary)
+    print(summary.BriefReport())
+    print(f'{x_ori} -> {x}')
+
+
+if __name__ == "__main__":
+    test_python_cost()
