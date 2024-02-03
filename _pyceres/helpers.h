1a7905d7dc1cb3395bf58aacbc90f718496353c3
Paul-Edouard Sarlin <15985472+sarlinpe@users.noreply.github.com>
2024-02-03 14:06:12 +0100

    Add missing ssize_t

--- _pyceres/helpers.h
+++ _pyceres/helpers.h
@@ -13,6 +13,13 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 
++#ifdef _MSC_VER
++#include <BaseTsd.h>
++typedef SSIZE_T ssize_t;
++#else
++#include <unistd.h>
++#endif
+
 using namespace pybind11::literals;
 namespace py = pybind11;
 
