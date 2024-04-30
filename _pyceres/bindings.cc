f90405e52597d565e7efb0d45ab4931be4081075
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2024-04-30 19:12:17 +0200

    Log ceres version

--- _pyceres/bindings.cc
+++ _pyceres/bindings.cc
@@ -14,6 +14,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(pyceres, m) {
   m.doc() = "PyCeres - Python bindings for the Ceres solver.";
   m.attr("__version__") = py::str(VERSION_INFO);
+  m.attr("__ceres_version__") = py::str(CERES_VERSION_STRING);
 
   py::add_ostream_redirect(m, "ostream_redirect");
   BindLogging(m);
