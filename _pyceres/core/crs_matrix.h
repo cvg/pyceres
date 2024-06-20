4e4c77ccd7dfb9c37d70ff4bdfec27ebc1272f9f
B1ueber2y <b1ueber2y@gmail.com>
2024-06-21 01:23:26 +0200

    update.

--- _pyceres/core/crs_matrix.h
+++ _pyceres/core/crs_matrix.h
@@ -24,16 +24,8 @@ py::tuple ConvertCRSToPyTuple(const ceres::CRSMatrix& crsMatrix) {
     }
   }
 
-  // convert std::vector data to py::array_t
-  py::array_t<int> rows_array(rows.size());
-  py::array_t<int> cols_array(cols.size());
-  py::array_t<double> values_array(values.size());
-  std::copy(rows.begin(), rows.end(), rows_array.mutable_data());
-  std::copy(cols.begin(), cols.end(), cols_array.mutable_data());
-  std::copy(values.begin(), values.end(), values_array.mutable_data());
-
   // return as a tuple
-  return py::make_tuple(rows_array, cols_array, values_array);
+  return py::make_tuple(rows, cols, values);
 }
 }  // namespace
 
