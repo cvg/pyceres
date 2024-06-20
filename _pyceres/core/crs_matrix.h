97319e626a0ae0d07ef8da0d227c33ce0c634b22
B1ueber2y <b1ueber2y@gmail.com>
2024-06-20 22:35:21 +0200

    formatting.

--- _pyceres/core/crs_matrix.h
+++ _pyceres/core/crs_matrix.h
@@ -4,9 +4,9 @@
 #include "_pyceres/helpers.h"
 #include "_pyceres/logging.h"
 
+#include <Eigen/Sparse>
 #include <ceres/ceres.h>
 #include <ceres/crs_matrix.h>
-#include <Eigen/Sparse>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
@@ -15,7 +15,7 @@ namespace {
 py::tuple ConvertCRSToPyTuple(const ceres::CRSMatrix& crsMatrix) {
   std::vector<int> rows, cols;
   std::vector<double> values;
-  
+
   for (int row = 0; row < crsMatrix.num_rows; ++row) {
     for (int k = crsMatrix.rows[row]; k < crsMatrix.rows[row + 1]; ++k) {
       rows.push_back(row);
@@ -32,23 +32,20 @@ py::tuple ConvertCRSToPyTuple(const ceres::CRSMatrix& crsMatrix) {
   std::copy(cols.begin(), cols.end(), cols_array.mutable_data());
   std::copy(values.begin(), values.end(), values_array.mutable_data());
 
-  //return as a tulple
+  // return as a tuple
   return py::make_tuple(rows_array, cols_array, values_array);
 }
-}
+}  // namespace
 
 void BindCRSMatrix(py::module& m) {
   using CRSMatrix = ceres::CRSMatrix;
   py::class_<CRSMatrix> PyCRSMatrix(m, "CRSMatrix");
   PyCRSMatrix.def(py::init<>())
-    .def_readonly("num_rows", &CRSMatrix::num_rows)
-    .def_readonly("num_cols", &CRSMatrix::num_cols)
-    .def_readonly("rows", &CRSMatrix::rows)
-    .def_readonly("cols", &CRSMatrix::cols)
-    .def_readonly("values", &CRSMatrix::values)
-    .def("to_tuple", [](CRSMatrix& self) {
-        return ConvertCRSToPyTuple(self);
-    });
+      .def_readonly("num_rows", &CRSMatrix::num_rows)
+      .def_readonly("num_cols", &CRSMatrix::num_cols)
+      .def_readonly("rows", &CRSMatrix::rows)
+      .def_readonly("cols", &CRSMatrix::cols)
+      .def_readonly("values", &CRSMatrix::values)
+      .def("to_tuple",
+           [](CRSMatrix& self) { return ConvertCRSToPyTuple(self); });
 }
-
-
--- _pyceres/core/problem.h
+++ _pyceres/core/problem.h
@@ -235,10 +235,10 @@ void BindProblem(py::module& m) {
              self.RemoveResidualBlock(residual_block_id.id);
            })
       .def("evaluate_jacobian",
-          [](ceres::Problem& self, const ceres::Problem::EvaluateOptions& options) {
-            ceres::CRSMatrix jacobian;
-            self.Evaluate(options, nullptr, nullptr, nullptr, &jacobian);
-            return jacobian;
-          }
-          );
+           [](ceres::Problem& self,
+              const ceres::Problem::EvaluateOptions& options) {
+             ceres::CRSMatrix jacobian;
+             self.Evaluate(options, nullptr, nullptr, nullptr, &jacobian);
+             return jacobian;
+           });
 }
